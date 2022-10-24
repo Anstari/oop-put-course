41fc83c658c47a0c53832a4cfa35c96639a8fc6a
Anstari <115485108+Anstari@users.noreply.github.com>
2022-10-24 18:22:02 +0200

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -1,74 +1,53 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
+class Cell_phone {
+public:
+    int c_id_cell;
+    string c_brand;
+    double c_price;
 
-class komputer{
-    public:
-        char marka[30];
-        int cena;
-        int moc;
-        int czyjestwsklepie;
+
+
+    Cell_phone(double price) {
+        c_id_cell = -1;
+        c_brand = "-1";
+        c_price = price;
+    };
+    Cell_phone(string brand) {
+        c_id_cell = -1;
+        c_brand = brand;
+        c_price = -1;
+    };
+    Cell_phone(int id_cell = 0, string brand = "Sumsung", double price = 100) {
+        c_id_cell = id_cell;
+        c_brand = brand;
+        c_price = price;
+    };
+
+    void print() {
+        cout << "id: " << c_id_cell << ", brand = " << c_brand << ", price: " << c_price << ";\n";
+    }
 };
 
-class gra{
-    public:
-        string nazwa;
-        int cena;
-        string opis;
-        int moc;
-};
 
-class ksiazka{
-    public:
-        char nazwa[30];
-        char rodzaj[30];
-        int cena;
-        string opis;
+class Twister {
+
 };
 
 int main()
 {
-    komputer k1;
-    cout<<"Prosze podac marke komputera 1: ";
-    cin>>k1.marka;
-    cout<<"Prosze podac cene komputera 1: ";
-    cin>>k1.cena;
-    cout<<"Prosze podac moc komputera 1 (1, 2 lub 3): ";
-    cin>>k1.moc;
-    cout<<"Czy komputer 1 jest w sklepie? (1-tak, 0-nie): ";
-    cin>>k1.czyjestwsklepie;
-    cout<<"Komputer 1: \nMarka: "<<k1.marka<<"\nCena:"<<k1.cena<<'\n';
-    if (k1.czyjestwsklepie == 0){
-        cout<<"Nie ma w sklepie\n";
-    }
-    else{
-        cout<<"Jest w sklepie\n";
-    }
-    
-    gra g1;
-    g1.nazwa = "Witcher";
-    g1.cena = 2;
-    g1.moc = 3;
-    
-    if (k1.moc>=g1.moc){
-        cout<<g1.nazwa<<" zadziala na komputerze "<<k1.marka;
-    }
-    else{
-        cout<<g1.nazwa<<" nie zadziala na komputerze "<<k1.marka;
-    }
-    
-    
-    
-    return 0;
-    
-    
-    
+    Cell_phone cel0;
+    cel0.print();
+
+    Cell_phone cel1(10.00);
+    cel1.print();
+
+    Cell_phone cel2("Nokia");
+    cel2.print();
+
+    Cell_phone cel3(3, "Xiaomi", 350.99);
+    cel3.print();
 }
+
