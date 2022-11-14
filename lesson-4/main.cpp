15a197cc839825f1ba375fa88de88f6ebe809da8
Anstari <115485108+Anstari@users.noreply.github.com>
2022-11-14 17:59:59 +0100

    Update main.cpp

--- lesson-4/main.cpp
+++ lesson-4/main.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
 
+using namespace std;
+
+class AbstractEmployee{
+    virtual void AskForPromotion()=0;
+};
+
+class Employee: AbstractEmployee{
+private:
+    string name;
+    string surname;
+    int age;
+    double salary;
+    int promoted = 0;
+public:
+    
+    void AskForPromotion(){
+        if ((age>=30) && (salary<3000)){
+          cout<< "Promoted\n";
+          promoted = 1;
+        } 
+        else cout<< "Promotion Declined\n";
+    };
+    
+    Employee Promote(double addSalary){
+        if (promoted = 1) return Employee(name, surname, age, salary+addSalary, 0);
+    }
+    
+    void ShowInfo(){
+        cout<<name<<' '<<surname<<' '<<age<<' '<<salary<<'\n';
+    }
+    
+    Employee(string name, string surname, int age, double salary, int promoted){
+        this->name=name;
+        this->surname=surname;
+        this->age=age;
+        this->salary=salary;
+        this->promoted=promoted;
+    };
+    
+};
+
+
+int main() {
+    Employee e1("Marcin", "Radom", 23, 4000, 0);
+    e1.AskForPromotion();
+    
+    Employee e2("Igor", "Oleszek", 35, 2499.00, 0);
+    e2.AskForPromotion();
+    
+    Employee e3 = e2.Promote(2000);
+    e3.ShowInfo();
+    return 0;
+}
