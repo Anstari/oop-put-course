176668ab13a4fd55c0350c5760338e2050a02792
Anstari <115485108+Anstari@users.noreply.github.com>
2023-01-09 11:23:48 +0100

    Add files via upload

---
+++ lesson-9/lesson-9.cpp
@@ -0,0 +1,33 @@
+﻿#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+class Game {
+public:
+    virtual string result() = 0;
+};
+
+class FakeFootballGame : public Game
+{
+public:
+    string result() {return "3:4";}
+};
+
+
+int main() {
+    FakeFootballGame footballGame;
+    try {
+        string score = footballGame.result();
+        if (score != "2:4") {
+            throw exception("Wrong score");
+        }
+        cout << score << '\n';
+    }
+    catch (const exception& exc) {
+        cout << exc.what() << endl;
+        return(1);
+    }
+
+    return(0);
+}
